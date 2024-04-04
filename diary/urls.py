5f7bf129a17234a9f2fa108e3adfd2a9e51a50f7
Jongsung Yoo <164390556+jos8304@users.noreply.github.com>
2024-04-03 22:04:55 -0400

    Create urls.py

---
+++ diary/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.index),
+    # path('diary/', views.page_list),
+    # path('diary/info/', views.info),
+    # path('diary/write', views.post_create),
+    # path('diary/<int:page_id>', views.page_detail),
+    # path('diary/<int:page_id>/edit', views.page_update),
+    # path('diary/<int:page_id>/delete', views.page_delete),
+]
