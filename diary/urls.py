e614556da1b0c606b836b2f611497d946bbef25f
jos8304 <jos8304@hotmail.com>
2024-04-04 22:07:29 -0400

    Create list and admin page

--- diary/urls.py
+++ diary/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('', views.index),
-    # path('diary/', views.page_list),
+    path('diary/', views.page_list),
     # path('diary/info/', views.info),
     # path('diary/write', views.post_create),
     # path('diary/<int:page_id>', views.page_detail),
