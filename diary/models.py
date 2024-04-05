9369efaf67b3ee6849b076a136181ecd0e88f2ac
jos8304 <jos8304@hotmail.com>
2024-04-04 21:27:34 -0400

    Created Page models

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    #title, content, create_date,updated_date
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    feeling = models.CharField(max_length=80)
+    score = models.IntegerField()
+    dt_created = models.DateField(verbose_name="Date Created", auto_now_add=True)
+    dt_modified = models.DateField(verbose_name="Date Modified", auto_now=True)
+
+    def __str__(self):
+        return self.title
