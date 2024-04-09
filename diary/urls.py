e06ade52a57948a502795ed95e5936428b18d9ca
jos8304 <jos8304@hotmail.com>
2024-04-08 21:23:37 -0400

    Create detail page and Connecting the list page with detail page

--- diary/urls.py
+++ diary/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('diary/', views.page_list),
     # path('diary/info/', views.info),
     # path('diary/write', views.post_create),
-    # path('diary/<int:page_id>', views.page_detail),
+    path('diary/<int:page_id>', views.page_detail, name="page_detail"),
     # path('diary/<int:page_id>/edit', views.page_update),
     # path('diary/<int:page_id>/delete', views.page_delete),
 ]
--- diary/views.py
+++ diary/views.py
@@ -6,3 +6,9 @@ def page_list(request):
     pages = Page.objects.all()
     context = {"pages" : pages}
     return render(request, 'diary/page_list.html', context)
+
+
+def page_detail(request, page_id):
+    pages = Page.objects.get(id=page_id)
+    context = {"page" : pages}
+    return render(request, 'diary/page_detail.html', context)
