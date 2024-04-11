b2f9dd06d45dd31a761db2c09275af27d7edf380
jos8304 <jos8304@hotmail.com>
2024-04-10 21:26:01 -0400

    Apply design

--- diary/views.py
+++ diary/views.py
@@ -12,3 +12,9 @@ def page_detail(request, page_id):
     pages = Page.objects.get(id=page_id)
     context = {"page" : pages}
     return render(request, 'diary/page_detail.html', context)
+
+
+def info(request):
+    pages = Page.objects.all()
+    context = {"pages" : pages}
+    return render(request, 'diary/info.html', context)
