a4d656033486cef1456681b8886c4eed6709d76b
jos8304 <jos8304@hotmail.com>
2024-04-22 21:49:23 -0400

    create model forms

--- diary/forms.py
+++ diary/forms.py
@@ -1,8 +1,8 @@
 from django import forms
+from .models import Page 
 
-class PageForm(forms.Form):
-    title = forms.CharField(max_length=100, label="Title")
-    content = forms.CharField(label="content", widget=forms.Textarea)
-    feeling = forms.CharField(max_length=80, label="feeling")
-    score = forms.IntegerField(label="score")
-    dt_created = forms.DateField(label="Date")
+class PageForm(forms.ModelForm):
+
+    class Meta:
+        model = Page 
+        fields = ['title', 'content', 'feeling', 'score']
--- diary/views.py
+++ diary/views.py
@@ -24,19 +24,8 @@ def info(request):
 
 def page_create(request):
     if request.method == 'POST':
-        title = request.POST['title']
-        content = request.POST['content']
-        feeling = request.POST['feeling']
-        score = request.POST['score']
-        dt_created = request.POST['dt_created']
-        new_page = Page(
-            title = title,
-            content = content,
-            feeling = feeling,
-            score = score,
-            dt_created = dt_created
-        )
-        new_page.save()
+        form = PageForm(request.POST)
+        new_page = form.save() 
         return redirect('page-detail', page_id=new_page.id)
     else:
         page_form = PageForm
