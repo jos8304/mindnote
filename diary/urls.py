65057ef30e50b2bfda8f66d5e484614b9dfa4ed5
Jason <jyou8304@gmail.com>
2024-04-25 10:54:19 -0400

    add to edit page

--- diary/urls.py
+++ diary/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('diary/info/', views.info, name="info"),
     path('diary/write', views.page_create, name="page-create"),
     path('diary/<int:page_id>', views.page_detail, name="page-detail"),
-    # path('diary/<int:page_id>/edit', views.page_update, name="page-update"),
+    path('diary/<int:page_id>/edit', views.page_update, name="page-update"),
     # path('diary/<int:page_id>/delete', views.page_delete, name="page-delete"),
 ]
--- diary/views.py
+++ diary/views.py
@@ -31,3 +31,15 @@ def page_create(request):
     else:
         form = PageForm()
     return render(request, 'diary/page_form.html', {'form': form})
+
+
+def page_update(request, page_id):
+    page = Page.objects.get(id=page_id)
+    if request.method == 'POST':
+        page_form = PageForm(request.POST,instance=page)
+        if page_form.is_valid():
+            page_form.save()
+            return redirect('page-detail', page_id=page.id)
+    else:        
+        page_form = PageForm(instance=page)
+        return render(request, 'diary/page_form.html', {'form': page_form})
